754b4d7fd4d35202598107744ee05760ea708c01
Bruno Pellizzetti <pelli.br@gmail.com>
2020-02-26 17:06:45 -0300

    Updates models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :quantity, presence: true, numericality: { only_integer: true }
 end
