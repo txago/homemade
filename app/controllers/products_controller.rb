21fbac3811c20be5a372618297d8b0e810d8193c
Bruno Pellizzetti <pelli.br@gmail.com>
2020-02-26 17:07:12 -0300

    Creates products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,41 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only ['show' 'destroy']
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @order = Order.new
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity, :photo)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
+
