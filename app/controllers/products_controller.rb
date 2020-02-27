a019391dbc915432b4dbd6b3490fca205160756b
Thiago Menezes <thiagorsm@gmail.com>
2020-02-27 17:08:42 -0300

    Creates views and controllers for user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
     @product.save
     if @product.save
       redirect_to product_path(@product)
