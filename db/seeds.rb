262d0cd9045340065b8c492423563daed37025a4
Thiago Menezes <thiagorsm@gmail.com>
2020-02-28 10:48:23 -0300

    Adds seeds and changes navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-products = Product.create([{ name: 'Bolo de Fuba'}, { description: 'Delicioso Bolo Feito em casa'}, { price: 1 }])
+require "faker"
+require "open-uri"
 
+Product.destroy_all
+User.destroy_all
 
+puts "Building fake users, products and orders"
+
+users_attributes = [
+  {
+    email: Faker::Internet.email,
+    id: 40,
+    password: "123456"
+  },
+  {
+    email: Faker::Internet.email,
+    id: 41,
+    password: "123456"
+  },
+  {
+    email: Faker::Internet.email,
+    id: 42,
+    password: "123456"
+  }
+]
+
+User.create!(users_attributes)
+
+10.times do
+  file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+  product = Product.new(
+    name: Faker::Book.title,
+    description: Faker::Lorem.paragraph_by_chars,
+    price: rand(1..100),
+    quantity: rand(1..100),
+    user_id: rand(40..42)
+  )
+  product.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  product.save!
+end
+
+puts "All done!"
